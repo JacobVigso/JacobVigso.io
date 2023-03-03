1f373a1f2cbdd81132d08f3eb2bf27ffb65c4f9c
Jacob Vigsoe <vigsojacob@gmail.com-->
2023-03-03 19:51:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const animationElements = document.querySelectorAll('.animation');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const element = entry.target;
+      const animations = element.getAttribute('data-animations').split(',');
+      let animationRules = '';
+
+      animations.forEach(animation => {
+        const [name, duration, timing, delay] = animation.trim().split(' ');
+        const rule = `${name} ${duration} ${timing} ${delay} forwards`;
+
+        if (animationRules) {
+          animationRules += ', ';
+        }
+
+        animationRules += rule;
+      });
+
+      element.style.animation = animationRules;
+      observer.unobserve(element);
+    }
+  });
+});
+
+animationElements.forEach(element => {
+  observer.observe(element);
+});
